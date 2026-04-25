d3fbde76edbbec67c7580ab08dcb805c5bca1451
Baku Shoda <bshoda@c5r2s6.42tokyo.jp>
2026-04-25 18:37:45 +0900

    add

--- ft_memset.c
+++ ft_memset.c
@@ -1,23 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bshoda <bshoda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/25 16:22:00 by bshoda            #+#    #+#             */
-/*   Updated: 2026/04/25 16:23:36 by bshoda           ###   ########.fr       */
+/*   Created: 2026/04/25 16:25:04 by bshoda            #+#    #+#             */
+/*   Updated: 2026/04/25 18:34:06 by bshoda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
-{
-	int	i;
+#include <stddef.h>
+// #include <stdio.h>
 
+// void	*ft_memset(void *a, int b, size_t len);
+
+// int	main(void)
+// {
+// 	char	str[] = "hello";
+
+// 	ft_memset(str, 'x', 5);
+// 	printf("%s", str);
+// 	return (0);
+// }
+
+void	*ft_memset(void *a, int b, size_t len)
+{
+	unsigned char	*p;
+	size_t			i;
+
+	p = (unsigned char *)a;
 	i = 0;
-	while (str[i])
+	while (i < len)
 	{
+		p[i] = (unsigned char)b;
 		i++;
 	}
-	return (i);
+	return (a);
 }
