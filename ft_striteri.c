1aed68268f2a366c208b2e9c1994bfb51255e94b
Baku Shoda <bshoda@c5r1s1.42tokyo.jp>
2026-05-09 14:23:57 +0900

    add striteri

---
+++ ft_striteri.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bshoda <bshoda@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/09 13:46:09 by bshoda            #+#    #+#             */
+/*   Updated: 2026/05/09 14:23:04 by bshoda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+// char	to_upper(unsigned int n, char *c);
+// void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+
+// int	main(void)
+// {
+// 	char	s[] = "abcde";
+
+// 	ft_striteri(s, to_upper);
+// 	return (0);
+// }
+
+// void	to_upper(unsigned int i, char *c)
+// {
+// 	(void)i;
+// 	if (*c >= 'a' && *c <= 'z')
+// 		*c -= 32;
+// }
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	int	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
