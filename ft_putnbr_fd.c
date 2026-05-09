7a1068491c86c0ddb7e8dcf77a3c7442618241ba
Baku Shoda <bshoda@c5r1s1.42tokyo.jp>
2026-05-09 16:30:30 +0900

    punbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,11 +6,43 @@
 /*   By: bshoda <bshoda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 15:55:07 by bshoda            #+#    #+#             */
-/*   Updated: 2026/05/09 15:56:53 by bshoda           ###   ########.fr       */
+/*   Updated: 2026/05/09 16:30:08 by bshoda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include <unistd.h>
+
+// void	ft_putnbr_fd(int n, int fd);
+
+// int	main(void)
+// {
+// 	int	n;
+// 	int	fd;
+
+// 	n = 5;
+// 	fd = 1;
+// 	ft_putnbr_fd(n, fd);
+// 	return (0);
+// }
+
+void	ft_putnbr_fd(int n, int fd)
 {
-	
+	char	c;
+
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		n = -n;
+		write(fd, "-", 1);
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+	}
+	c = (n % 10) + '0';
+	write(fd, &c, 1);
 }
