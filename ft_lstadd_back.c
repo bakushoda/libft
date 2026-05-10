dd2f9b3e7704a5ea2ea8cff3696ab4022f2a0af6
Baku Shoda <bshoda@c5r1s4.42tokyo.jp>
2026-05-10 19:36:17 +0900

    lstadd_back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bshoda <bshoda@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/10 17:31:31 by bshoda            #+#    #+#             */
+/*   Updated: 2026/05/10 19:35:56 by bshoda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*current;
+
+	if (!lst || !new)
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	current = *lst;
+	while (current->next)
+		current = current->next;
+	current->next = new;
+}
+
+// int	main(void)
+// {
+// 	t_list	*head;
+// 	t_list	*new1;
+// 	t_list	*new2;
+
+// 	head = ft_lstnew("hello");
+// 	new1 = ft_lstnew("world");
+// 	ft_lstadd_back(&head, new1);
+// 	new2 = ft_lstnew("42");
+// 	ft_lstadd_back(&head, new2);
+// 	printf("%s\n", (char *)head->content);
+// 	printf("%s\n", (char *)head->next->content);
+// 	printf("%s\n", (char *)head->next->next->content);
+// 	return (0);
+// }
