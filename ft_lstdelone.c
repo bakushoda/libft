80c55b99d72a55665a73fc0989b3d334306e33da
Baku Shoda <bshoda@c5r1s4.42tokyo.jp>
2026-05-10 19:54:00 +0900

    lstdelone

---
+++ ft_lstdelone.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bshoda <bshoda@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/10 19:36:55 by bshoda            #+#    #+#             */
+/*   Updated: 2026/05/10 19:53:35 by bshoda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
