2cae013c8ef27000c2dced57608ebc67eaa68566
Baku Shoda <bshoda@c5r1s2.42tokyo.jp>
2026-05-17 20:53:33 +0900

    fix prototype

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bshoda <bshoda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 21:00:00 by bshoda            #+#    #+#             */
-/*   Updated: 2026/05/12 20:17:36 by bshoda           ###   ########.fr       */
+/*   Updated: 2026/05/17 20:53:17 by bshoda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
-char	*ft_strnstr(const char *haystack, const char *needle,
+char	*ft_strnstr(const char *big, const char *little,
 			size_t len);
 
 int		ft_atoi(const char *str);
